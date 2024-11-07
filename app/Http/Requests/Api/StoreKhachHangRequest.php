4dea0cff725ed67629924fccd20de2f10981620a
mintam04 <mjntem2004@gmail.com>
2024-11-07 20:54:16 +0700

    register

---
+++ app/Http/Requests/Api/StoreKhachHangRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+
+class StoreKhachHangRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'ho_ten' => 'required|string|max:255',
+            'email' => 'required|email',
+            'dien_thoai' => 'required|string|max:15',
+            'dia_chi' => 'nullable|string|max:255',
+            'mat_khau' => 'required|min:8',
+        ];
+    }
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors();
+
+        $response = response()->json([
+            'errors' => $errors->messages(),
+        ], Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        throw new HttpResponseException($response);
+    }
+}
