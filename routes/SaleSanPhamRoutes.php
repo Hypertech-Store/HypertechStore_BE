0c1491d8808e628acaabee18dfaeaa8e07f68884
mintam04 <mjntem2004@gmail.com>
2024-11-15 11:28:50 +0700

    get sale san pham

--- routes/SaleSanPhamRoutes.php
+++ routes/SaleSanPhamRoutes.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\Api\SaleSanPhamController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/sale-san-pham', [SaleSanPhamController::class, 'addSale']);
+Route::post('/sale-san-pham/add-sale', [SaleSanPhamController::class, 'addSale']);
+Route::get('/sale-san-pham/get-sale', [SaleSanPhamController::class, 'getSaleSanPhams']);
