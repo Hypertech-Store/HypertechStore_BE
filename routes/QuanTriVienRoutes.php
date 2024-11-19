6b768bf7851dd10c42d7100c8bc57a1594d5dcb9
mintam04 <mjntem2004@gmail.com>
2024-11-19 12:51:30 +0700

    login quan tri vien

--- routes/QuanTriVienRoutes.php
+++ routes/QuanTriVienRoutes.php
@@ -11,4 +11,5 @@ Route::prefix('quan-tri-viens')->group(function () {
     Route::patch('/{id}/toggle-active', [QuanTriVienController::class, 'toggleActive']); // Bật/tắt
     Route::get('/getAll', [QuanTriVienController::class, 'index']); // Lấy danh sách
     Route::get('/detail/{id}', [QuanTriVienController::class, 'show']); // Lấy chi tiết
+    Route::post('/login', [QuanTriVienController::class, 'login']);
 });
