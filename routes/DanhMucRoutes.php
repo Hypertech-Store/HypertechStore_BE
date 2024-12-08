1bb224f0fa6966a22bc9536eeb61e64ef50ddde9
maintt <nguyenmine0803@gmail.com>
2024-12-08 22:11:57 +0700

    add api to categori

--- routes/DanhMucRoutes.php
+++ routes/DanhMucRoutes.php
@@ -3,4 +3,19 @@
 use App\Http\Controllers\Api\DanhMucController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('danh-muc', DanhMucController::class);
+Route::prefix('danh-muc')->group(function() {
+    // Hiển thị danh sách các danh mục
+    Route::get('/getAll', [DanhMucController::class, 'index']);
+
+    // Tạo mới danh mục
+    Route::post('/addNew', [DanhMucController::class, 'store']);
+
+    // Hiển thị chi tiết một danh mục
+    Route::get('/detail/{id}', [DanhMucController::class, 'show']);
+
+    // Cập nhật danh mục
+    Route::put('/update/{id}', [DanhMucController::class, 'update']);
+
+    // Xóa danh mục
+    Route::delete('/delete/{id}', [DanhMucController::class, 'destroy']);
+});
