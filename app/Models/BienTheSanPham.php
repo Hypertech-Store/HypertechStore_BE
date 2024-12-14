72df901a8a7b6397d52c693f0591eb9317bd2b90
mintam04 <mjntem2004@gmail.com>
2024-12-15 00:39:08 +0700

    sua chi tiet san pham

--- app/Models/BienTheSanPham.php
+++ app/Models/BienTheSanPham.php
@@ -26,4 +26,14 @@ class BienTheSanPham extends Model
     {
         return $this->belongsToMany(GiaTriThuocTinh::class, 'lien_ket_bien_the_va_gia_tri_thuoc_tinhs', 'bien_the_san_pham_id', 'gia_tri_thuoc_tinh_id');
     }
+
+    public function lienKetBienTheVaGiaTri()
+    {
+        return $this->hasMany(LienKetBienTheVaGiaTriThuocTinh::class, 'bien_the_san_pham_id', 'id');
+    }
+
+    public function hinhAnhSanPhams()
+    {
+        return $this->hasMany(HinhAnhSanPham::class, 'lien_ket_bien_the_va_gia_tri_thuoc_tinh_id', 'id');
+    }
 }
