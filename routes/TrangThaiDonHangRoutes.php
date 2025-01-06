e7716de2f4ad0e84dd91b1420bd3970783d802a6
mintam04 <mjntem2004@gmail.com>
2025-01-06 19:19:04 +0700

    route ttdh

--- routes/TrangThaiDonHangRoutes.php
+++ routes/TrangThaiDonHangRoutes.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\Api\TrangThaiDonHangController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('trang-thai-don-hang', TrangThaiDonHangController::class);
-Route::get('/trang-thai-don-hang/getAllTrangThaiDonHang', [TrangThaiDonHangController::class, 'getAll']);
+Route::get('/getAllTrangThaiDonHang', [TrangThaiDonHangController::class, 'getAll']);
